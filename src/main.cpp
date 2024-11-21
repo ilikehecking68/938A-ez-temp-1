624548971c1fbd22a8ea36bb17ec9516bc5f5c36
Rishit K <nobody.rk2222@gmail.com>
2024-11-21 11:03:08 -0800

    removed pto stuff and made arm 2 5.5 motors

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include "main.h"
 
-pros::Motor pto(14, pros::MotorGears::blue, pros::MotorEncoderUnits::degrees);
 pros::Motor intake(21, pros::MotorGears::blue, pros::MotorEncoderUnits::degrees);
-pros::MotorGroup intake_group({14, 21}, pros::MotorGears::blue, pros::MotorEncoderUnits::degrees);
-bool pto_piston_status = false;
-pros::ADIDigitalOut pto_piston(2, false);
 bool doinker_status = false;
 pros::ADIDigitalOut doinker(1, false);
 bool mogo_status = false;
-pros::ADIDigitalOut mogo(4, false);
+pros::ADIDigitalOut mogo(3, false);
+pros::MotorGroup arm_motors({2, 10}, pros::MotorGears::green, pros::MotorEncoderUnits::degrees);
 pros::Rotation arm_sensor(10);
 #define arm_sensor_get_degrees()((double)((arm_sensor).get_position() / 100))
 
@@ -36,10 +33,10 @@ ez::Drive chassis(
  */
 void initialize() {
   // Print our branding over your terminal :D
-  pto.set_brake_mode(pros::MotorBrake::hold);
+  arm_motors.set_brake_mode(pros::MotorBrake::hold);
+  arm_motors.tare_position_all();
   arm_sensor.reverse();
   arm_sensor.reset_position();
-  pto.tare_position();
   ez::ez_template_print();
 
   pros::delay(500);  // Stop the user from doing anything while legacy ports configure
@@ -116,11 +113,66 @@ void autonomous() {
 
   chassis.pid_drive_set(-23, 120);
   chassis.pid_wait();
-  //put down mogo
-  //turn on intake
+  
+  mogo.set_value(mogo_status);
+  intake.move(127);
 
   //chassis.pid_swing_set(ez::LEFT_SWING, 180, 120, 5);
   //chassis.pid_wait();
+  
+  chassis.pid_turn_set(140, 100);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(26, 50);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(-6, 120);
+  chassis.pid_wait();
+
+  chassis.pid_turn_set(127, 110);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(12, 50);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(-15, 120);
+  chassis.pid_wait();
+
+  intake.move(0);
+
+  chassis.pid_turn_set(37, 110);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(46, 95);
+  chassis.pid_wait();
+
+  doinker.set_value(true);
+
+  chassis.pid_turn_set(95, 50);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(-5, 70);
+  chassis.pid_wait();
+
+  chassis.pid_turn_set(145, 127);
+  chassis.pid_wait();
+  
+  doinker.set_value(false); //doinker in
+  arm_motors.move_relative(170, 50); //move arm
+
+  chassis.pid_drive_set(-72, 127); //touch bar
+  chassis.pid_wait();
+  //red left side
+  /*void red_left_side() {
+  chassis.pid_targets_reset();                // Resets PID targets to 0
+  chassis.drive_imu_reset();                  // Reset gyro position to 0
+  chassis.drive_sensor_reset();               // Reset drive sensors to 0
+  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
+
+  chassis.pid_drive_set(-23, 120);
+  chassis.pid_wait();
+  mogo.set_value(true);
+  intake_group.move(127);
   
   chassis.pid_turn_set(140, 100);
   chassis.pid_wait();
@@ -146,7 +198,7 @@ void autonomous() {
   chassis.pid_drive_set(46, 95);
   chassis.pid_wait();
 
-  //doinker out
+  doinker.set_value(true);
 
   chassis.pid_turn_set(95, 50);
   chassis.pid_wait();
@@ -156,6 +208,62 @@ void autonomous() {
 
   chassis.pid_turn_set(145, 127);
   chassis.pid_wait();
+*/
+//red right side
+/*
+void red_right_side() {
+  chassis.pid_targets_reset();                // Resets PID targets to 0
+  chassis.drive_imu_reset();                  // Reset gyro position to 0
+  chassis.drive_sensor_reset();               // Reset drive sensors to 0
+  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
+
+  chassis.pid_drive_set(-50, 120);
+  chassis.pid_wait();
+
+  mogo.set_value(true);
+  pto_piston.set_value(false);
+  intake_group.move(127);
+
+  pros::delay(1000);
+  mogo.set_value(false);
+  
+  chassis.pid_turn_set(-150, 100);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(-23, 120);
+  chassis.pid_wait();
+
+  mogo.set_value(true);
+
+  chassis.pid_turn_set(30, 80);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(20, 100);
+  chassis.pid_wait();
+
+  chassis.pid_turn_set(160, 100);
+  chassis.pid_wait();
+
+  mogo.set_value(false);
+
+  chassis.pid_drive_set(50, 120);
+  chassis.pid_wait();
+
+  // pick up red ring and load it into wall stake mech
+
+  chassis.pid_turn_set(60, 80);
+  chassis.pid_wait();
+
+  chassis.pid_drive_set(15, 80);
+  chassis.pid_wait();
+
+  //score ring onto alliance stake
+
+  chassis.pid_drive_set(-40, 120);
+  chassis.pid_wait();
+}
+
+*/
 }
 
 /**
@@ -172,10 +280,6 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 
-void set_pto_piston(bool new_status){
-  pto_piston_status = new_status;
-  pto_piston.set_value(pto_piston_status);
-}
 
 void doinker_update(bool button_new_press){
   if (button_new_press){
@@ -199,14 +303,8 @@ ez::PID arm_pid(4.2, 0, 20, 0, "Arm");
 
 bool arm_pid_running = false;
 void arm_update(bool up_button_held, bool load_button_new_press, bool noninterfere_button_new_press){
-  if (up_button_held || noninterfere_button_new_press || load_button_new_press){
-    intake_group.move(0);
-    if (!pto_piston_status){
-      set_pto_piston(true);
-    }
-  }
   if (up_button_held){
-    pto.move(-127);
+    arm_motors.move(-127);
     arm_pid_running = false;
   } else if (load_button_new_press){
     arm_target = arm_loading;
@@ -218,22 +316,19 @@ void arm_update(bool up_button_held, bool load_button_new_press, bool noninterfe
     arm_pid_running = true;
   }
   if (arm_pid_running){
-    pto.move(-arm_pid.compute(arm_sensor_get_degrees()));
+    arm_motors.move(-arm_pid.compute(arm_sensor_get_degrees()));
   } else if (!up_button_held){
-    pto.move(0);
+    arm_motors.move(0);
   }
 }
 
 void intake_update(bool in_button_held, bool out_button_held){
-  if ((in_button_held || out_button_held) && pto_piston_status){
-    set_pto_piston(false);
-  }
   if (in_button_held){
-    intake_group.move(127);
+    intake.move(127);
   } else if (out_button_held){
-    intake_group.move(-127);
+    intake.move(-127);
   } else {
-    intake_group.move(0);
+    intake.move(0);
   }
 }
 
@@ -243,7 +338,6 @@ void opcontrol() {
 
   chassis.drive_brake_set(driver_preference_brake);
   arm_pid.exit_condition_set(80, 2, 250, 8, 500, 500);
-  arm_target = arm_noninterfere;
   while (true) {
     // PID Tuner
     // After you find values that you're happy with, you'll have to set them in auton.cpp
@@ -270,10 +364,9 @@ void opcontrol() {
     // chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
     // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
     intake_update(master.get_digital(DIGITAL_R2), master.get_digital(DIGITAL_R1));
-    arm_update(master.get_digital(DIGITAL_LEFT/*B*/), master.get_digital_new_press(DIGITAL_RIGHT), master.get_digital_new_press(DIGITAL_DOWN));
+    arm_update(master.get_digital(DIGITAL_B), master.get_digital_new_press(DIGITAL_RIGHT), master.get_digital_new_press(DIGITAL_DOWN));
     mogo_update(master.get_digital_new_press(DIGITAL_Y));
     doinker_update(master.get_digital_new_press(DIGITAL_L1));
-    pros::lcd::print(2, pto_piston_status ? "true" : "false");
     pros::lcd::print(3, "%lf", (double)(arm_sensor.get_position() / 100));
     
     // . . .
